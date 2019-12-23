a277c087dab9299bcf5de9550ccb37ae873e4adc
Daniel Rolnik <danrol92@gmail.com>
2019-12-23 16:20:14 +0200

    small upd

--- ex3.py
+++ ex3.py
@@ -1,3 +1,11 @@
+import librosa
+import scipy.stats as stats
+
+fpath = "test_files/00d4b9cd-3619-482a-9941-b5dc2b277cc5.wav"
+y, sr = librosa.load(fpath, sr=None)
+mfcc = librosa.feature.mfcc(y=y, sr=sr)
+mfcc = stats.zscore(mfcc, axis=1) # Normalization
+
 def main():
     pass
 
